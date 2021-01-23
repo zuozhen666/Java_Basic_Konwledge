50eec8f9b2a79b39275f225f780e3e1023045d7a
zuozhen666 <1774098040@qq.com>
2021-01-23 13:59:13 +0800

    update Generic

--- Generic/src/com/zuozhen/generic/GenericTest.java
+++ Generic/src/com/zuozhen/generic/GenericTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -26,14 +27,52 @@ public class GenericTest {
         自然排序和定制排序之后也要指明类型
          */
     }
+
     @Test
-    public void test1(){
+    public void test1() {
         //实例化时没有指明类的泛型，默认为Object
         //要求：最好都要在实例化时指明泛型的类型
         Order order = new Order();
         order.setOrderT(123);
 
-        Order<String> order1 = new Order<>("AA","BB",123);
+        Order<String> order1 = new Order<>("AA", "BB", 123);
         System.out.println(order1);
+
+        //测试泛型方法
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("aa");
+        strings.add("bb");
+        //调用时指明泛型参数的类型
+        List list = order.copyFromArrayToList(strings);
+        System.out.println(list);
     }
+
+    /*
+    1.泛型在继承方面的体现：A是B的父类，但是G<A>和G<B>无子父类关系，为并列关系
+    //A<G>是B<G>的父类
+     */
+    /*
+    通配符的使用：
+    通配符：?
+     */
+    @Test
+    public void test2() {
+        List<Object> list1 = null;
+        List<String> list2 = null;
+        List<?> list = null;//可以视为通用父类
+        list = list1;//均可
+        list = list2;//均可
+        //不能向list添加数据，除了null
+        //允许读取数据，类型为Object
+    }
+
+    public void print(List<?> list) {
+        //...
+    }
+    /*
+    有限制条件的通配符的使用
+    G<? extends Xxx>   <=
+    G<? super Xxx>     >=
+    //可以读取或写入，但是有限制，均与面向对象相关
+     */
 }
--- Generic/src/com/zuozhen/generic/Order.java
+++ Generic/src/com/zuozhen/generic/Order.java
@@ -1,5 +1,8 @@
 package com.zuozhen.generic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * 泛型结构-》泛型类、接口；
  */
@@ -32,5 +35,16 @@ public class Order<T> {
                 ", orderName='" + orderName + '\'' +
                 ", orderId=" + orderId +
                 '}';
+    }
+
+    //泛型方法：在方法中出现了泛型的结构，泛型参数与类的泛型参数没有任何关系
+    //泛型方法所属的类是不是泛型类都没有关系
+    //不同于泛型类，泛型方法可以声名为静态
+    public <E> List<E> copyFromArrayToList(ArrayList<E> arr) {
+        ArrayList<E> list = new ArrayList<>();
+        for (E e : arr) {
+            list.add(e);
+        }
+        return list;
     }
 }

a48c1eb12506a8fa9c9b057fc1c5f44a0933c215
zuozhen666 <1774098040@qq.com>
2021-02-09 11:30:54 +0800

    Wrapper

--- OOP/src/zuozhen/ObjectTest.java
+++ OOP/src/zuozhen/ObjectTest.java
@@ -1,5 +1,19 @@
 package zuozhen;
-
+/*
+==与equals()
+//==，运算符
+1.可以使用在基本数据类型和引用数据类型变量中
+2.如果比较的是基本数据类型变量，则比较两个变量保存数据是否相等（类型不一定相同）
+3.如果比较的是引用数据类型变量，比较两个对象地址值是否相同
+//equals()方法
+1.只适用于引用数据类型
+2.Object类中定义的equals方法和“==”作用相同
+3.String,Date,File,包装类等都重写了equals()方法，重写后比较的是两个对象的“内容”是否相同
+ */
+/*
+toString()
+//当输出一个对象的引用时，实际上就是调用toString()方法
+ */
 public class ObjectTest {
     /*
     所有java类的根父类
@@ -7,11 +21,11 @@ public class ObjectTest {
 
      */
     public static void main(String[] args) {
-        Test test = new Test();
+        TestO test = new TestO();
         System.out.println(test.getClass().getSuperclass());//class java.lang.Object
     }
 }
 
-class Test {
+class TestO {
 
 }
---
+++ OOP/src/zuozhen/Wrapper.java
@@ -0,0 +1,108 @@
+package zuozhen;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.junit.Test;
+
+import javax.xml.transform.Source;
+
+/*
+包装类：针对八种基本数据类型定义相应的引用类型
+
+ */
+public class Wrapper {
+    @Test
+    public void test1() {
+        /*
+        基本数据类型-》包装类，调用包装类的构造器
+         */
+        int num = 10;
+        Integer integer = new Integer(num);
+        System.out.println(integer);//10(Integer重写了toString方法)
+        System.out.println(new Integer("123"));//123
+        System.out.println(new Float("12.7"));//12.7
+        System.out.println(new Boolean(true));//true
+        System.out.println(new Boolean("True"));//true
+        System.out.println(new Boolean("true123"));//false
+    }
+
+    @Test
+    public void test2() {
+        /*
+        包装类-》基本数据类型：调用XxxValue()
+         */
+        Integer in1 = new Integer(21);
+        int val = in1.intValue();
+        System.out.println(val + 1);//22
+    }
+
+    /*
+    jdk5.0加入新特性：
+    1.自动装箱：
+    2.自动拆箱：
+     */
+    @Test
+    public void test3() {
+        /*
+        自动装箱与自动拆箱
+         */
+        Integer in1 = 21;
+        System.out.println(in1 + 1);//22
+    }
+
+    @Test
+    public void test4() {
+        /*
+        基本数据类型，包装类->String
+         */
+        int num = 10;
+        //方式一
+        String str = "" + num;
+        //方式二(调用ValueOf)
+        float f1 = 12.3f;
+        String s = String.valueOf(f1);
+        System.out.println(s);//12.3
+    }
+
+    @Test
+    public void test5() {
+        /*
+        String->基本数据类型，包装类(调用parseXxx())
+         */
+        String str = "123";
+        int num = Integer.parseInt(str);
+        System.out.println(num);//123
+    }
+
+    @Test
+    public void test6() {
+        /*
+        面试题
+         */
+        Object obj = true ? new Integer(1) : new Double(2.0);//三元运算符，类型自动提升（编译时）
+        System.out.println(obj);//1.0
+        /*
+        面试题
+         */
+        Object o2;
+        if (true)
+            o2 = new Integer(1);
+        else
+            o2 = new Double(2.0);
+        System.out.println(o2);//1
+        /*
+        面试题
+         */
+        Integer i = new Integer(1);
+        Integer j = new Integer(1);
+        System.out.println(i == j);//false
+        Integer m = 1;
+        Integer n = 1;
+        System.out.println(m == n);//true
+        /*
+        -128~127(内部类IntegerCache里有一个数组，包含了常用的数值，用于提升效率)
+         */
+        Integer x = 128;//相当于new了一个Integer对象
+        Integer y = 128;
+        System.out.println(x == y);//false
+    }
+}
